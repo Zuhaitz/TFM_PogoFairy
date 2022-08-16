f6f9a11a44e54bbf34b78d7593894aaef94a84f8
Jorge Pastor Duran <95531829+JorgePastorDuran@users.noreply.github.com>
2022-08-16 15:19:58 +0200

    SpawnRandomActors

---
+++ Source/TFM_PogoFairy/EnemySpawner.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class TFM_POGOFAIRY_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	UPROPERTY(EditAnywhere)
+		int amountEnemies = 1;
+
+	UPROPERTY(EditAnywhere)
+		TArray<AActor*> spawnPointsEnemies;
+
+	UPROPERTY(EditAnywhere)
+		TArray<TSubclassOf<AActor>> blueprintsToSpawn;
+};
